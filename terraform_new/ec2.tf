46eaf29b108b06e8462320204c99f8c0b4163d42
global jasamant <jasamant.sikarwar@opstree.com>
2020-05-24 16:43:20 +0530

    24

--- terraform_new/ec2.tf
+++ terraform_new/ec2.tf
@@ -12,7 +12,7 @@
 #
 #  ami                    = "${var.ec2_ami}"
 #  instance_type          = "${var.ec2_instance_type}"
-#  key_name               = "rocky"
+#  key_name               = "jasamant"
 #  monitoring             = false
 #  vpc_security_group_ids = ["${aws_security_group.bastion_sg.id}"]
 #  subnet_id              = "${module.public_subnet_1.subnet_id}"
